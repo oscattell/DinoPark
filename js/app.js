349cb8c60d802f7fe2979da1f0290b5a337bb6c9
Oscar Cattell <os.web@cattell.ch>
2024-02-05 21:10:19 +0100

    Initial Commit

---
+++ js/app.js
@@ -0,0 +1,51 @@
+kaboom()
+
+add([
+  pos(2500, 1),
+  rect(4, 1800),
+  outline(4),
+  area(),
+])
+
+add([
+  pos(50, 1),
+  rect(4, 1800),
+  outline(4),
+  area(),
+])
+
+
+
+add([
+  pos(500, 900),
+  rect(1500, 150),
+  outline(4),
+  area(),
+  color(255, 0, 0),
+])
+
+add([
+  pos(500, 1000),
+  rect(100, 430),
+  outline(4),
+  area(),
+  color(255, 0, 0),
+])
+
+add([
+  pos(1850,520),
+  rect(150, 430),
+  outline(4),
+  area(),
+  color(255, 0, 0),
+])
+
+add([
+  pos(1850,200),
+  rect(1500),
+  outline(4),
+  area(),
+  color(255, 0, 0),
+])
+
+
